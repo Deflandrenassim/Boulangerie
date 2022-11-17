6a7b83e87c1492335bd04be80b83fac250f7f13a
Deflandrenassim <nassim.def@gmail.com>
2022-11-17 09:47:55 +0100

    fe

--- src/container/Flour.jsx
+++ src/container/Flour.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ButtonContainer from './ButtonContainer'
-
-import Count from '../component/Count'
-import { useMoney } from '../context/MoneyContext'
+import ButtonContainer from './ButtonContainer';
+import Pain from '../img/painChocolat.jpeg';
+import Tarte from '../img/tarte.png'
+import Count from '../component/Count';
 import Sale from './Sale';
 import Macaron from '../img/macaron.png'
 import Croiss from '../img/croissant.png';
@@ -10,26 +10,24 @@ import FlourBatch from './FlourBatch';
 
 
 const Flour = () => {
-    const { money } = useMoney();
 
     return (
         <div>
-            {money >= 3 ? (
-                <div>
-                    <ButtonContainer />
-                    <Count />
-                    <Sale img={Croiss} value={5} price={1} />
-                    <Sale img={Macaron} value={8} price={3} />
-                    <FlourBatch valueMoney={3} valueFloor={10} />
 
-                </div>
-            ) : (<div>
+
+            <div>
                 <ButtonContainer />
                 <Count />
-                <Sale img={Croiss} value={5} price={1} />
+                <Sale img={Croiss} value={3} price={1} />
+                <Sale img={Pain} value={5} price={2} />
                 <Sale img={Macaron} value={8} price={3} />
+                <Sale img={Tarte} value={8} price={15} />
+                <FlourBatch valueMoney={3} valueFloor={3} lot="3" />
+                <FlourBatch valueMoney={45} valueFloor={50} lot="50" />
+                <FlourBatch valueMoney={55} valueFloor={100} lot="100" />
             </div>
-            )}
+
+
 
         </div>
 
